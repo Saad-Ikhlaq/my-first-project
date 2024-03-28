61bc2a8f02df195760336910e229098b4b8340d7
Saad-Ikhlaq <mohammad.saad7991@gmail.com>
2024-03-28 23:28:45 +0500

    text change

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
       <div className="hero-content-container">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
-          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          YOUR FEET DESERVE THE BEST and WE’RE HERE TO HELP YOU WITH OUR
           SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.
         </p>
